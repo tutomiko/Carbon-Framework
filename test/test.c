8ccf032f83d8cf802c813f16195927f99905c739
tutomiko <98809576+tutomiko@users.noreply.github.com>
2022-11-02 17:11:36 +0200

    Add files via upload

---
+++ test/test.c
@@ -0,0 +1,31 @@
+#
+#pragma region <imports>
+#pragma region "export header imports"
+
+#pragma endregion
+#
+#pragma region "platform-dependent imports"
+
+#pragma endregion
+#
+#pragma region "platform-independent imports"
+#include <stdio.h>
+#pragma endregion
+#
+#pragma region "3rd-party imports"
+#include "../include/fireworks.h"
+#pragma endregion
+#
+#pragma region "shared imports"
+
+#pragma endregion
+#
+#pragma region "local imports"
+
+#pragma endregion         
+#pragma endregion
+#
+#
+int _cdecl main(int argc, char** argv) {
+
+}
