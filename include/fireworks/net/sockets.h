ecb50ef21ffead375ecd4317a40d5bed9f67f7bf
tutomiko <98809576+tutomiko@users.noreply.github.com>
2022-11-02 17:14:18 +0200

    Add files via upload

---
+++ include/fireworks/net/sockets.h
@@ -0,0 +1,57 @@
+/*
+ * FileName: string.h
+ *
+ * Written by tutomiko <https://www.github.com/tutomiko>
+ *
+ * Copyright (c) 2022, Tuomas Kontiainen
+ *
+ * This file is a part of Fireworks Framework <https://github.com/tutomiko/Fireworks-Framework>
+ * Fireworks Framework is a free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later versions.
+ *
+ * Fireworks Framework is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fireworks Framework. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+#
+#pragma once
+#
+#pragma region <imports>
+#pragma region "platform-dependent imports"
+
+#pragma endregion
+#
+#pragma region "platform-independent imports"
+
+#pragma endregion
+#
+#pragma region "3rd-party imports"
+
+#pragma endregion
+#
+#pragma region "shared imports"
+#include <fwbase.h>
+#include <fireworks/net/socket.h>
+#pragma endregion
+#
+#pragma region "local imports"
+
+#pragma endregion
+#pragma endregion
+#
+#
+FWIMPORT  
+void sockets_cleanup(void);
+
+
+FWIMPORT __success(return == true)
+bool_t sockets_init(void);
+
+
